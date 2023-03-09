6e121967d4c3c873b0971d347e4e5e09ae962988
looloolalaa <dlrlgus65@nate.com>
2023-03-09 16:31:08 +0900

    label in uiview (contentSize)

--- UIKitPractice/FirstViewController.swift
+++ UIKitPractice/FirstViewController.swift
@@ -11,18 +11,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myView = MyView()
-        myView.data = .init(labelStr: "Hello World!", buttonStr: "click")
-        
-        self.view.addSubview(myView)
-        
-        myView.translatesAutoresizingMaskIntoConstraints = false
-        myView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100).isActive = true
-        myView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -150).isActive = true
-        myView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -200).isActive = true
-        myView.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        
-        
+        print("hello")
     }
     
 }
