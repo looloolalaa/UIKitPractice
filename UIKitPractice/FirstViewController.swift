4862cf10058aa073047cc59b85d481b22c509a92
looloolalaa <dlrlgus65@nate.com>
2023-10-04 20:22:09 +0900

    pr test

--- UIKitPractice/FirstViewController.swift
+++ UIKitPractice/FirstViewController.swift
@@ -7,132 +7,30 @@
 
 import UIKit
 
-enum Section {
-    case feed, board, post
-}
-
-class Feed: Hashable {
-    let id: UUID
-    let imageName: String
-    
-    init(imageName: String) {
-        self.id = UUID()
-        self.imageName = imageName
-    }
-    
-    static func == (lhs: Feed, rhs: Feed) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
-
-
-
 class FirstViewController: UIViewController {
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    
-    @IBOutlet weak var myCollectionView: UICollectionView!
-    var dataSource: UICollectionViewDiffableDataSource<Section, Feed>!
-    
-    var datas = [
-        "moon", "zzz", "sparkles", "cloud", "tornado", "smoke.fill", "tv.fill", "gamecontroller", "headphones", "flame", "bolt.fill", "hare", "tortoise", "moon", "zzz", "sparkles", "cloud", "tornado", "smoke.fill", "tv.fill", "gamecontroller", "headphones", "flame", "bolt.fill", "hare", "tortoise", "ant", "hare", "car", "airplane", "heart", "bandage", "waveform.path.ecg", "staroflife", "bed.double.fill", "signature", "bag", "cart", "creditcard", "clock", "alarm", "stopwatch.fill", "timer"
-    ].map { Feed(imageName: $0) }
-    
+//    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.myCollectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        textView.delegate = self
+        textView.isScrollEnabled = false
         
-        self.myCollectionView.delegate = self
-//        self.myCollectionView.dataSource = self
+        let label = UILabel()
+        label.frame = CGRect(x: textView.frame.minX, y: textView.frame.maxY + 100, width: 50, height: 100)
+        label.text = "asd"
+        self.view.addSubview(label)
         
-        self.dataSource = UICollectionViewDiffableDataSource<Section, Feed>(collectionView: self.myCollectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Feed) -> UICollectionViewCell? in
-            
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
-            cell.myLabel.text = "!" + self.datas[indexPath.item].imageName + "!"
-            cell.myImage.image = UIImage(systemName: self.datas[indexPath.item].imageName)
-            
-            return cell
-        }
-        
-        updateSnapshot()
-        
-        
-        
-        self.myCollectionView.collectionViewLayout = createCompositionalLayout()
-        
+//        print(textView.bottomAnchor)
     }
-    
-    func updateSnapshot() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Feed>()
-        snapshot.appendSections([.feed])
-        snapshot.appendItems(datas, toSection: .feed)
-        
-        dataSource.apply(snapshot)
-    }
-    
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        switch sender {
-        case minusButton:
-            if !self.datas.isEmpty {
-                self.datas.removeFirst()
-            }
-        case resetButton:
-//            self.datas = []
-            self.datas[0] = Feed(imageName: "pencil")
-        case plusButton:
-            self.datas.append(Feed(imageName: "pencil"))
-        default:
-            break
-        }
-//        self.myCollectionView.reloadData()
-        updateSnapshot()
-    }
-    
 }
 
+extension FirstViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        textView.frame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
 
-//extension FirstViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return self.datas.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
-//        cell.myLabel.text = datas[indexPath.item]
-//        cell.myImage.image = UIImage(systemName: datas[indexPath.item])
-//        return cell
-//    }
-//}
-
-
-
-extension FirstViewController: UICollectionViewDelegate {
-    
-}
-
-extension FirstViewController {
-    func createCompositionalLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
-
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .absolute(150))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 3)
-            
-            let section = NSCollectionLayoutSection(group: group)
-            return section
-        }
-        
-        return layout
     }
 }
