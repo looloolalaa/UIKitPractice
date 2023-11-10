92b061339cbe75fb6d97068e489f9509d83f55b0
looloolalaa <dlrlgus65@nate.com>
2023-11-10 21:27:41 +0900

    Apple Login Test

--- UIKitPractice/FirstViewController.swift
+++ UIKitPractice/FirstViewController.swift
@@ -6,30 +6,108 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class FirstViewController: UIViewController {
-//    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var textView: UITextView!
     
+    // MARK: - Properties
+    let authorizationAppleIDButton = ASAuthorizationAppleIDButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - UI
+    private func configureUI() {
+        setAdditionalPropertyAttributes()
+        setConstraints()
+    }
+
+    private func setAdditionalPropertyAttributes() {
+        authorizationAppleIDButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButton(_:)), for: .touchUpInside)
+    }
+
+    private func setConstraints() {
+        view.addSubview(authorizationAppleIDButton)
+        authorizationAppleIDButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            authorizationAppleIDButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            authorizationAppleIDButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    // MARK: - Selectors
+    @objc private func handleAuthorizationAppleIDButton(_ sender: ASAuthorizationAppleIDButton) {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+//        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
         
-        textView.delegate = self
-        textView.isScrollEnabled = false
+        controller.delegate = self
+        controller.presentationContextProvider = self
         
-        let label = UILabel()
-        label.frame = CGRect(x: textView.frame.minX, y: textView.frame.maxY + 100, width: 50, height: 100)
-        label.text = "asd"
-        self.view.addSubview(label)
-        
+        controller.performRequests()
+    }
+
+}
+
+extension FirstViewController: ASAuthorizationControllerDelegate {
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // Create an account in your system.
+            
+            let userIdentifier = appleIDCredential.user
+            let userFirstName = appleIDCredential.fullName?.givenName
+            let userLastName = appleIDCredential.fullName?.familyName
+            let userEmail = appleIDCredential.email
+            print("!apple response!")
+            if let asd = appleIDCredential.identityToken {
+                print("identityToken: ", String(data: asd, encoding: .utf8 ))
+            }
+            if let zzz = appleIDCredential.authorizationCode {
+                print("authorizationCode: ", String(data: zzz, encoding: .utf8 ))
+            }
+            
+//            print(userIdentifier)
+            print(userFirstName)
+            print(userLastName)
+            print(userEmail)
+            
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
+            appleIDProvider.getCredentialState(forUserID: userIdentifier) { (credentialState, error) in
+                switch credentialState {
+                case .authorized:
+                    // The Apple ID credential is valid. Show Home UI Here
+                    print("authorized")
+                    break
+                case .revoked:
+                    // The Apple ID credential is revoked. Show SignIn UI Here.
+                    print("rovoked")
+                    break
+                case .notFound:
+                    // No credential was found. Show SignIn UI Here.
+                    print("notFound")
+                    break
+                default:
+                    print("default")
+                    break
+                }
+            }
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error)
     }
 }
 
-extension FirstViewController: UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-        let fixedWidth = textView.frame.size.width
-        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-        textView.frame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
 
+extension FirstViewController: ASAuthorizationControllerPresentationContextProviding {
+
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
     }
 }
